61b3f84a1bf2fec1306b9d3b076eb9e3ffa53297
spedroas <spedroas@gmail.com>
2025-05-07 06:21:56 -0300

    AplicandoRepRemote

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,16 +3,16 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
           // Criando um objeto Scanner para ler a entrada do usuário
           Scanner scanner = new Scanner(System.in);
-          // Solicitando e lendo um texto
+          // Solicitando um texto
           System.out.print("\nPor favor, digite seu nome: ");
           String textoNome = scanner.nextLine();
-        // Solicitando e lendo um valor inteiro
+        // Solicitando um texto
           System.out.print("\nPor favor, Digite o número de sua Agência sem o dígito: ");
           String numeroAgencia = scanner.nextLine();
-        // 
+        // Solicitando um inteiro
           System.out.print("\nPor favor, Digite o número de sua Conta: ");
           int numeroConta = scanner.nextInt();
-         //
+         //Solicitando um double
           System.out.print("\nDigite o valor a ser depositado: ");
           double numeroSaldo = scanner.nextDouble();
           scanner.nextLine(); // Limpar o buffer do teclado
